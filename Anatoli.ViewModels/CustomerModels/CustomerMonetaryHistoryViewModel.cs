a8eb153edfaa5516cb9fe8295d17564401357f0f
Asal Razavi <razaviasal@gmail.com>
2016-06-27 02:40:32 -0800

    Customer history

---
+++ Anatoli.ViewModels/CustomerModels/CustomerMonetaryHistoryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Anatoli.ViewModels.CustomerModels
+{
+    public class CustomerMonetaryHistoryViewModel : BaseViewModel
+    {
+        public Guid CustomerId { get; set; }
+
+        public string UserId { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+        public string TransactionPDate { get; set; }
+
+        public string TransactionDesc { get; set; }
+
+        public decimal Amount { get; set; }
+        public decimal NetAmount { get; set; }
+        public decimal Discount { get; set; }
+
+        public string PlaceDesc { get; set; }
+
+        public string TerminalDesc { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Anatoli.ViewModels/CustomerModels/CustomerNonMonetaryHistoryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Anatoli.ViewModels.CustomerModels
+{
+    public class CustomerNonMonetaryHistoryViewModel : BaseViewModel
+    {
+        public Guid CustomerId { get; set; }
+
+        public string UserId { get; set; }
+ 
+        public DateTime ActivityDate { get; set; }
+        public string ActivityPDate { get; set; }
+
+        public string ActivityDesc { get; set; }
+
+        public string PlaceDesc { get; set; }
+
+        public string TerminalDesc { get; set; }
+
+        public string Description { get; set; }
+
+
+    }
+}
