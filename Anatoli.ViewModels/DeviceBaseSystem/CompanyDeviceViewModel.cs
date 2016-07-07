283fd03401e234b7c89c02ea910cab2c2e2255bd
Asal Razavi <razaviasal@gmail.com>
2016-07-06 22:48:14 -0800

    Gis Company device

---
+++ Anatoli.ViewModels/DeviceBaseSystem/CompanyDeviceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anatoli.ViewModels.DeviceBaseSystem
+{
+    public class CompanyDeviceViewModel : BaseViewModel
+    {
+        public Guid CompanyId { get; set; }
+        public Guid DeviceModelId { get; set; }
+        public string DeviceModelName { get; set; }
+        public string IMEI { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ Anatoli.ViewModels/RequestModel/DeviceBaseSystem/CompanyDeviceRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anatoli.ViewModels.DeviceBaseSystem;
+
+namespace Anatoli.ViewModels.RequestModel.DeviceBaseSystem
+{
+    public class CompanyDeviceRequestModel : BaseRequestModel
+    {
+        public CompanyDeviceViewModel companyDeviceData { get; set; }
+
+    }
+}
